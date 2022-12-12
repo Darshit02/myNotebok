b30b4d2743b37b2eb25c1e467ea4e93a64c32b75
Darshit <darshitmistry2@gmail.com>
2022-12-12 12:05:37 +0530

    Connectintg script to database and setup backend

---
+++ backend/db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, () => {
+        console.log("Connected to Mongo Successfully");
+    })
+}
+module.exports = connectToMongo;
---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const connectToMongo = require('./db');
+const express = require('express')
+connectToMongo();
+const app = express()
+const port = 3000
+app.get('/', (req, res) => {
+    res.send('Hello Darshit')
+})
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
