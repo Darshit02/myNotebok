3dd5c08a2d843d343b94da991eb7dba1bc033d7a
Darshit <darshitmistry2@gmail.com>
2023-01-25 17:09:39 +0530

    Updating Endpoint for Nots Was inited

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -5,7 +5,7 @@ const Note = require("../models/Note");
 // const fetchuser = require('../middleware/fetchuser');
 const { body, validationResult } = require("express-validator");
 
-// ROUTE 1: Get All the Notes using: GET "/api/auth/getuser". Login required
+// ROUTE 1: Get All the Notes using: GET "/api/notes/getuser". Login required
 router.get("/fetchallnotes", fetchuser, async (req, res) => {
   try {
     const notes = await Note.find({ user: req.user.id });
@@ -15,29 +15,66 @@ router.get("/fetchallnotes", fetchuser, async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
-// ROUTE 2: Add a new Note using: POST "/api/auth/addnote". Login required
-router.post('/addnote', fetchuser, [
-  body('title', 'Enter a valid title').isLength({ min: 3 }),
-  body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),], async (req, res) => {
-      try {
+// ROUTE 2: Add a new Note using: POST "/api/notes/addnote". Login required
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "Description must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
 
-          const { title, description, tag } = req.body;
-
-          // If there are errors, return Bad request and the errors
-          const errors = validationResult(req);
-          if (!errors.isEmpty()) {
-              return res.status(400).json({ errors: errors.array() });
-          }
-          const note = new Note({
-              title, description, tag, user: req.user.id
-          })
-          const savedNote = await note.save()
-
-          res.json(savedNote)
-
-      } catch (error) {
-          console.error(error.message);
-          res.status(500).send("Internal Server Error");
+      // If there are errors, return Bad request and the errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
       }
-  })
+      const note = new Note({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+      const savedNote = await note.save();
+
+      res.json(savedNote);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+// ROUTE 3: Add a new Note using: POST "/api/notes/updatenote". Login required
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body;
+  // create A New Note Object
+  const newNote = {};
+  if (title) {
+    newNote.title = title;
+  }
+  if (description) {
+    newNote.description = description;
+  }
+  if (tag) {
+    newNote.tag = tag;
+  }
+
+  //  Find The Note to be updated and update it 
+  let note  = await Note.findById(req.params.id);
+  if(!note){
+   return res.status(404).send("Not Found")
+  }
+  if(note.user.toString() !== req.user.id){
+    return res.status(401).send("Not Allowed")
+  }
+  note = await Note.findByIdAndUpdate(req.params.id , {$set: newNote} , {new:true})
+  res.json({note});
+
+});
+
 module.exports = router;
