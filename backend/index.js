d2b0c9caef299decc984b3a721f6b9262745d6bf
Darshit <darshitmistry2@gmail.com>
2023-01-25 14:22:10 +0530

    fixing som fetch problem and adding notes backpoint

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
 const connectToMongo = require('./db');
 const express = require('express')
+var cors = require('cors') 
 
 connectToMongo();
 const app = express()
 const port = 5000
 
+app.use(cors())
 app.use(express.json())
-app.get('/', (req, res) => {
-    res.send('Hello')
-})
 app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 
 app.listen(port, () => {
