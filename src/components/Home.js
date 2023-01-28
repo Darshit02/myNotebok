a41518f8d4f78f788a5b0072b6f437122b0a378e
Darshit <darshitmistry2@gmail.com>
2023-01-28 22:46:42 +0530

    fixing some issues

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Notes from "./Notes";
-const Home = (props) => {
-  const {showAlert} = props
+
+const Home = () => {
   return (
     <div>
-     <Notes showAlert={showAlert}/>
+     <Notes />
     </div>
   );
 };
